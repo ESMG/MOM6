6f7e335c2b77b0045c30dcdc2a1444ff76c62b19
Rob Cermak <rob.cermak@gmail.com>
2020-08-15 08:36:44 +0000

    Add a way to run doxygen on the remote RTD server.

--- docsNew/conf.py
+++ docsNew/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess
+subprocess.call('doxygen doxygen.conf', shell=True)
 
 # -- Project information -----------------------------------------------------
 
